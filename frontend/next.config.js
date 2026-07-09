2d706e1a3f430f8d8473bf1cedc6f75a192a59f7
Khalid <mainacyrus089@gmail.com>
2026-07-09 16:21:16 +0300

    --ammend

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -9,17 +9,6 @@ const nextConfig = {
       },
     ],
   },
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          { key: 'Cache-Control', value: 'no-store, no-cache, must-revalidate' },
-          { key: 'CDN-Cache-Control', value: 'no-store' },
-        ],
-      },
-    ]
-  },
 }
 
 module.exports = nextConfig
